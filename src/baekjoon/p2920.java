003fd4c8d315d5be8dc8a78423dbef5e1048e115
608-18 <608-18@608-18-PC>
2019-01-22 16:25:09 +0900

    clear_level6

---
+++ src/baekjoon/p2920.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p2920 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] ascending = {1, 2, 3, 4, 5, 6, 7, 8};
+		int[] descending = {8, 7, 6, 5, 4, 3, 2, 1};
+		
+		String[] str = in.nextLine().split(" ");
+		int[] number = new int[str.length];
+		for (int i = 0; i < str.length; i++) {
+			number[i] = Integer.parseInt(str[i]);
+		}
+		if (Arrays.equals(number, ascending)) {
+			System.out.println("ascending");
+		}
+		else if (Arrays.equals(number, descending)) {
+			System.out.println("descending");
+		}
+		else {
+			System.out.println("mixed");
+		}
+		
+		in.close();
+	}
+}
