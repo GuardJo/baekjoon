c3952a15bd55facc4adb1c2077691a52695b7f89
GuardJo <dagh1218@naver.com>
2019-11-12 20:06:01 +0900

    2019-11-12 finished

---
+++ src/baekjoon/p10818.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p10818 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        int size = Integer.parseInt(str);
+        int[] num = new int[size];
+
+        str = in.nextLine();
+        String[] numbers = str.split(" ");
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(numbers[i]);
+        }
+        Arrays.sort(num);
+        System.out.println(num[0] + " " + num[size-1]);
+        in.close();
+    }
+}
---
+++ src/baekjoon/p3052.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p3052 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = 10;
+        int mod = 42;
+        int[] number = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int num = in.nextInt();
+            number[i] = num%mod;
+        }
+        Arrays.sort(number);
+        int count = 1;
+        for (int i = 1; i < number.length; i++) {
+            if (number[i-1] != number[i]) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        in.close();
+    }
+}
