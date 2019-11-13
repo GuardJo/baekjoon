074c96919737498a743f28e1c3b8657051924501
GuardJo <dagh1218@naver.com>
2019-11-13 18:17:58 +0900

    2019-11-13 finished

---
+++ src/baekjoon/p2292.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class p2292 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int index = in.nextInt();
+        int room = 6;
+        int result = 1;
+
+        int count = 1;
+        while (index > result) {
+            result = result + (room * count);
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
