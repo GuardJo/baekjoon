b418d00d1ff04ea1615573b794ca6d1f5da6f90e
GuardJo <dagh1218@naver.com>
2020-07-02 22:12:24 +0900

    p10816 finish

--- src/baekjoon/p10816.java
+++ src/baekjoon/p10816.java
@@ -4,41 +4,42 @@ import java.io.*;
 import java.util.Arrays;
 
 public class p10816 {
-    private static int searchIndex (int[] arr, int n, int start, int end) {
-        int mid = (end + start) / 2;
-        if (end - start < 2) {
-            return 0;
+    private static int searchLower (int[] arr, int n, int start, int end) {
+        int mid = 0;
+        while (start < end) {
+            mid = (end + start) / 2;
+            if (arr[mid] < n) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
         }
-        if (n == arr[mid]) {
-            return mid;
-        }
-        else if (n < arr[mid]) {
-            return searchIndex(arr, n, start, mid-1);
+        if (arr[end] != n) {
+            return -1;
         }
         else {
-            return searchIndex(arr, n, mid+1, end);
+            return end;
         }
     }
-    private static int numberCount (int[] arr, int n, int index) {
-        int count = 1;
-        for (int i = index+1; i < arr.length; i++) {
-            if (arr[i] != n) {
-                break;
-            }
-            else {
-                count++;
-            }
-        }
-        for (int i  = index-1; i >= 0; i--) {
-            if (arr[i] != n) {
-                break;
-            }
-            else {
-                count++;
+
+    private static int searchUpper (int[] arr, int n, int start, int end) {
+        int mid = 0;
+        while (start < end) {
+            mid = (end + start) / 2;
+            if (arr[mid] <= n) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
-        return count;
+        if (arr[end] == n) {
+            return end + 1;
+        }
+        else {
+            return end;
+        }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -55,11 +56,19 @@ public class p10816 {
 
         int selectCount = Integer.parseInt(br.readLine());
 
+        int[] counts = new int[selectCount];
         s = br.readLine();
         str = s.split(" ");
         for (int i = 0; i < selectCount; i++) {
             int num = Integer.parseInt(str[i]);
-            bw.write(numberCount(cards, num, searchIndex(cards, num, 0, cards.length-1)));
+            int lower = searchLower(cards, num, 0, cards.length-1);
+            if (lower >= 0) {
+                int upper = searchUpper(cards, num, 0, cards.length-1);
+                counts[i] = upper - lower;
+            }
+        }
+        for (int i = 0; i < counts.length; i++) {
+            bw.write(counts[i] + " ");
         }
         bw.write("\n");
 
