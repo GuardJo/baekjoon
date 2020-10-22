0af5f845930d09029dfa68bffa3c37e50a7bd18b
GuardJo <dagh1218@naver.com>
2020-10-22 13:40:29 +0900

    Fix to NHN PreTest1 Simulation

--- src/NHN_PreTest1/p1.java
+++ src/NHN_PreTest1/p1.java
@@ -2,31 +2,33 @@ package NHN_PreTest1;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class p1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int matSize = Integer.parseInt(br.readLine());
-        int[][] mat = new int[matSize][matSize];
+        int sizeOfMatrix = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[sizeOfMatrix][sizeOfMatrix];
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < matSize; i++) {
+        for (int i = 0; i < sizeOfMatrix; i++) {
             String[] str = br.readLine().split(" ");
-            for (int j = 0; j < matSize; j++) {
-                mat[i][j] = Integer.parseInt(str[j]);
+            for (int j = 0; j < sizeOfMatrix; j++) {
+                matrix[i][j] = Integer.parseInt(str[j]);
             }
         }
 
-        for(int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
-                if (mat[i][j] == 1) {
-                    mat[i][j] = 0;
-                    list.add(searchPlace(mat, i, j, 1));
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = 0;
+                    list.add(searchPlace(matrix, i, j, 1));
                 }
             }
         }
 
         System.out.println(list.size());
+        Collections.sort(list);
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
