5a88bbca3b7245292489692b887ded3e2a7cf078
Kyeongho Jo <dagh1218@naver.com>
2020-10-24 10:26:49 +0900

    Fix MakeCompany Class in kakao2021

--- src/kakao2021/MakeCompany.java
+++ src/kakao2021/MakeCompany.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class MakeCompany {
     public static void main(String[] args) {
         int[] sales = {};
-        int[][] links = {{10, 8}, {1, 9}, {9, 7}, [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]];
+        int[][] links = {{10, 8}, {1, 9}, {9, 7}, {5, 4}, {1, 5}, {5, 10}, {10, 6}, {1, 3}, {10, 2}};
 
         int result = solution(sales, links);
     }
