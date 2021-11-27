26a1ba17b536ba3241701a0c8ddf6a90a9bf7444
kyeongho <dagh1218@gmail.com>
2021-11-27 16:01:45 +0900

    Naver FinTech CodingTest

---
+++ src/LineFinTech/p3.java
@@ -0,0 +1,17 @@
+package LineFinTech;
+
+/**
+ * 관람객 배정 (미완)
+ */
+public class p3 {
+    public static int[] solution(int n, int k) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] result = solution(5, 12);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
