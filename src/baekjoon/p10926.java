1aa2110b6b962f54d6bc0bd22e10a3da4d554dea
kyeongho <dagh1218@naver.com>
2023-07-12 22:32:50 +0900

    백준 - 입출력과 사칙연산 문제 풀이
    - 백준 : 단계별 풀어보기
    - 입출력과 사칙연산 문제 풀이
    -- 10926, 11382, 18108 문제 풀이

---
+++ src/baekjoon/p10926.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+
+/**
+ * <h1>??!</h1>
+ * <p>백준 문제 : 10926번</p>
+ * <a href="https://www.acmicpc.net/problem/10926">문제 링크</a>
+ */
+public class p10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String answer = solution(str);
+
+        bw.write(answer);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static String solution(String str) {
+        String postFix = "??!";
+
+        return str + postFix;
+    }
+}
